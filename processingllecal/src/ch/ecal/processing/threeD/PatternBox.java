0283fbfff6f5a041a39d77f58faa7c589ab7a21c
elisemigraine <elisemigraine@gmail.com>
2015-09-11 13:52:11 +0200

    dégradé de cube

---
+++ processingllecal/src/ch/ecal/processing/threeD/PatternBox.java
@@ -0,0 +1,25 @@
+package ch.ecal.processing.threeD;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class PatternBox {
+	PVector color; 
+	PApplet p;
+	
+	public PatternBox(PApplet p) {
+		this.p = p;
+		color = new PVector(255, 255, 255);
+	}
+	
+	
+	public void draw() {
+		p.fill(color.x, color.y, color.z);
+		p.box(1);
+	}
+	
+
+	
+	
+	
+}
